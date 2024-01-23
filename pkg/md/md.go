2093b042f8c9e0e3710de74f1ccf2987e2fb4ac7
Dustin Firebaugh <dafirebaugh@gmail.com>
2024-01-22 22:36:27 -0500

    - adding page links to headers - updating nav btns to sides of content - minor style updates to sidebar

--- pkg/md/md.go
+++ pkg/md/md.go
@@ -2,19 +2,40 @@ package md
 
 import (
 	"bytes"
+	"fmt"
 
 	"github.com/alecthomas/chroma/v2/formatters/html"
 	mathjax "github.com/litao91/goldmark-mathjax"
 	"github.com/yuin/goldmark"
 	emoji "github.com/yuin/goldmark-emoji"
 	highlighting "github.com/yuin/goldmark-highlighting/v2"
+	"github.com/yuin/goldmark/ast"
 	"github.com/yuin/goldmark/extension"
 	"github.com/yuin/goldmark/parser"
+	"github.com/yuin/goldmark/renderer"
 	rhtml "github.com/yuin/goldmark/renderer/html"
+	"github.com/yuin/goldmark/util"
 	"go.abhg.dev/goldmark/hashtag"
 	"go.abhg.dev/goldmark/mermaid"
 	"mvdan.cc/xurls/v2"
 )
+
+type CustomHeadingRenderer struct{}
+
+func (r *CustomHeadingRenderer) RegisterFuncs(reg renderer.NodeRendererFuncRegisterer) {
+	reg.Register(ast.KindHeading, r.renderHeading)
+}
+
+func (r *CustomHeadingRenderer) renderHeading(w util.BufWriter, source []byte, node ast.Node, entering bool) (ast.WalkStatus, error) {
+	if entering {
+		heading := node.(*ast.Heading)
+		id := node.Attributes()[0].Value
+		fmt.Fprintf(w, "<h%d id=\"%s\"><a href=\"#%s\" class=\"header-anchor\">%s", heading.Level, id, id, node.Text(source))
+		fmt.Fprintf(w, "</a></h%d>", heading.Level)
+		return ast.WalkSkipChildren, nil
+	}
+	return ast.WalkContinue, nil
+}
 
 func ToHTML(source []byte) []byte {
 	markdown := goldmark.New(
@@ -50,6 +71,9 @@ func ToHTML(source []byte) []byte {
 			parser.WithAutoHeadingID(),
 		),
 		goldmark.WithRendererOptions(
+			renderer.WithNodeRenderers(
+				util.Prioritized(&CustomHeadingRenderer{}, 500),
+			),
 			rhtml.WithHardWraps(),
 			rhtml.WithXHTML(),
 			rhtml.WithUnsafe(),
