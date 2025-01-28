00beaa1416120bf9ca32e87466cd39f49b5ba395
dfirebaugh <dfirebaugh@users.noreply.github.com>
2025-01-28 22:17:44 +0000

    deploy: f1b53b0182079bc58455c1f54b0e4eed6edbaa17

--- js/nav-btns.js
+++ js/nav-btns.js
@@ -1,83 +1,91 @@
-
-const template = document.createElement('template');
+const template = document.createElement("template");
 template.innerHTML = `
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <link 
+    rel="stylesheet" 
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
+    integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" 
+    crossorigin="anonymous" 
+    referrerpolicy="no-referrer" 
+  />
 
-<style>
-a {
-    position:fixed;
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    width: 5vw;
-    justify-content: center;
-}
+  <style>
+    :host {
+      display: inline-block;
+    }
 
-a:hover {
-    background-color: var(--secondary-background-color);
-    cursor: pointer;
-}
+    .nav-wide-wrapper {
+      position: fixed;
+      width: 4rem;
+      height: 4rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
 
-a {
-    color: var(--primary-color);
-}
+    a {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      width: 100%;
+      height: 100%;
+      text-decoration: none;
+      color: var(--primary-color);
+    }
 
-.nav-wide-wrapper {
-    display: flex;
-}
-</style>
+    .nav-wide-wrapper:hover {
+      background-color: var(--secondary-background-color);
+      cursor: pointer;
+    }
+  </style>
 
-<span class="nav-wide-wrapper" aria-label="Page navigation">
-    <a rel="prev" class="nav-chapters previous" title="Previous chapter" aria-label="Previous chapter"
-        aria-keyshortcuts="Left">
-        <i class="fa fa-angle-left"></i>
+  <span class="nav-wide-wrapper">
+    <a rel="prev" class="nav-chapters previous" title="Previous chapter" aria-label="Previous chapter" aria-keyshortcuts="Left">
+      <i class="fa fa-angle-left"></i>
     </a>
-    <a rel="next" class="nav-chapters next" title="Next chapter" aria-label="Next chapter"
-        aria-keyshortcuts="Right">
-        <i class="fa fa-angle-right"></i>
+    <a rel="next" class="nav-chapters next" title="Next chapter" aria-label="Next chapter" aria-keyshortcuts="Right">
+      <i class="fa fa-angle-right"></i>
     </a>
-</span>`;
+  </span>
+`;
 
 class NavBtns extends HTMLElement {
-    static get observedAttributes() {
-        return ['nav-type', 'href', 'theme'];
+  static get observedAttributes() {
+    return ["nav-type", "href"];
+  }
+
+  constructor() {
+    super();
+    const shadowRoot = this.attachShadow({ mode: "open" });
+    shadowRoot.appendChild(template.content.cloneNode(true));
+  }
+
+  connectedCallback() {
+    this.updateLinks();
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue !== newValue) {
+      this.updateLinks();
     }
+  }
 
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' }).appendChild(template.content.cloneNode(true));
+  updateLinks() {
+    const navType = this.getAttribute("nav-type");
+    const href = this.getAttribute("href");
+    const prevLink = this.shadowRoot.querySelector(".previous");
+    const nextLink = this.shadowRoot.querySelector(".next");
+
+    if (navType === "prev") {
+      nextLink?.remove();
+      if (prevLink) prevLink.href = href || "#";
+    } else if (navType === "next") {
+      prevLink?.remove();
+      if (nextLink) nextLink.href = href || "#";
+    } else {
+      if (prevLink) prevLink.href = "#";
+      if (nextLink) nextLink.href = "#";
     }
-
-    connectedCallback() {
-        this.updateLinks();
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        if (oldValue !== newValue) {
-            this.updateLinks();
-        }
-    }
-
-    updateLinks() {
-        const navType = this.getAttribute('nav-type');
-        const href = this.getAttribute('href');
-        const prev = this.shadowRoot.querySelector('.previous');
-        const next = this.shadowRoot.querySelector('.next');
-        const wrapper = this.shadowRoot.querySelector('.nav-wide-wrapper');
-
-
-        if (navType === 'prev') {
-            next?.remove();
-            wrapper.style.justifyContent = 'left';
-            if (prev) prev.href = href;
-        } else if (navType === 'next') {
-            prev?.remove();
-            wrapper.style.justifyContent = 'right';
-            if (next) next.href = href;
-        }
-    }
+  }
 }
 
-window.customElements.define('bb-navbtn', NavBtns);
+window.customElements.define("bb-navbtn", NavBtns);
